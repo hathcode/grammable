8efdec31c9d1e1d8e405085cdf090910731bd7c0
Kevin Hathorn <hathornk@hotmail.com>
2016-07-18 13:56:27 +0000

    Secured edit, update, destroy actions, refactored code

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,19 +1,19 @@
 class GramsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create,]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
 def destroy
     @gram = Gram.find_by_id(params[:id])
       return render_not_found if @gram.blank?
-    @gram.delete
+      return render_not_found(:forbidden) if @gram.user != current_user
+    @gram.destroy
     redirect_to root_path
     end
 
 def update
     @gram = Gram.find_by_id(params[:id])
       return render_not_found if @gram.blank?
-
+      return render_not_found(:forbidden) if @gram.user != current_user
     @gram.update_attributes(gram_params)
-
     if @gram.valid?
       redirect_to root_path
     else
@@ -24,6 +24,7 @@ def update
   def edit
     @gram = Gram.find_by_id(params[:id])
       return render_not_found if @gram.blank?
+      return render_not_found(:forbidden) if @gram.user != current_user
   end
 
 
@@ -54,7 +55,7 @@ def update
     params.require(:gram).permit(:message)
   end
 
-  def render_not_found
-    render text: 'Not Found :(', status: :not_found
+  def render_not_found(status=:not_found)
+    render text: "#{status.to_s.titleize} :(", status: status
   end
 end
