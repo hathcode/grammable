900257d231003fd9569b2c5250a84f38d40e0e97
Kevin Hathorn <hathornk@hotmail.com>
2016-08-03 17:34:39 +0000

    Testing completed, added styles and finishing touches

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -56,7 +56,4 @@ def update
     params.require(:gram).permit(:message, :picture)
   end
 
-  def render_not_found(status=:not_found)
-    render text: "#{status.to_s.titleize} :(", status: status
-  end
 end
